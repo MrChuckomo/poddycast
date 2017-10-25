5a33a7a5b22ce6ad52c205d1e4b9d38642953826
Alexander Kettler <alexander.kettler91@gmail.com>
2017-10-25 18:48:11 +0200

    First search results
    # get back the first test results from apples itues api
    # display results in html body
    # set an input for future search results

--- electron-v1-poddycast/Electron.app/Contents/Resources/app/js/itunes.js
+++ electron-v1-poddycast/Electron.app/Contents/Resources/app/js/itunes.js
@@ -1,4 +1,39 @@
+
+// https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/#overview
+// https://itunes.apple.com/search?term=freakshow&media=podcast
+
+var https = require('https');
+
 function getPodcasts()
 {
+    var options =
+    {
+        host: 'itunes.apple.com',
+        port: 443,
+        path: '/search?term=freakshow&media=podcast',
+        method: 'GET',
+    };
 
+    var req = https.request(options, function(res)
+    {
+        // console.log('STATUS: ' + res.statusCode);
+        // console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', function (chunk)
+        {
+            // console.log('BODY: ' + chunk);
+
+            document.getElementById("content-body").innerHTML += chunk
+        });
+    });
+
+    req.on('error', function(e)
+    {
+        console.log('problem with request: ' + e.message);
+    });
+
+    // write data to request body
+    req.write('data\n');
+    req.write('data\n');
+    req.end();
 }
