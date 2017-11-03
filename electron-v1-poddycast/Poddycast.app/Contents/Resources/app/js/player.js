043c95f17447c75b0ff01047362289920df65b8e
Alexander Kettler <alexander.kettler91@gmail.com>
2017-11-03 11:55:52 +0100

    Audio Player Adjustments
    + image to forward 30 sec
    + image to replay 30 sec
    + new js file for audio player handling
    # toggle between play and pause

---
+++ electron-v1-poddycast/Poddycast.app/Contents/Resources/app/js/player.js
@@ -0,0 +1,33 @@
+
+const s_Pause =
+`
+    <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
+    <path d="M0 0h24v24H0z" fill="none"/>
+`
+
+const s_Play =
+`
+    <path d="M8 5v14l11-7z"/>
+    <path d="M0 0h24v24H0z" fill="none"/>
+`
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+function playPause(_Self)
+{
+    togglePlayPauseButton(_Self)
+}
+
+function togglePlayPauseButton(_Button)
+{
+    if (_Button.getAttribute("mode") == "play")
+    {
+        _Button.innerHTML = s_Pause
+        _Button.setAttribute("mode", "pause")
+    }
+    else
+    {
+        _Button.innerHTML = s_Play
+        _Button.setAttribute("mode", "play")
+    }
+}
