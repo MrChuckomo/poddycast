197190f6a2403e26d539ee418e7e8b6dd08c73ca
Alexander Kettler <alexander.kettler91@gmail.com>
2017-11-18 14:37:24 +0100

    Save Audio Progress
    # just save every 10 sec.

--- electron-v1-poddycast/Poddycast.app/Contents/Resources/app/js/player.js
+++ electron-v1-poddycast/Poddycast.app/Contents/Resources/app/js/player.js
@@ -76,8 +76,6 @@ function playNow(_Self)
     {
         togglePlayPauseButton(document.getElementById("play-pause"))
     }
-
-
 }
 
 function playPause()
@@ -119,7 +117,12 @@ function updateProgress()
     var Player       = document.getElementById("player")
     var PlayerSource = Player.getElementsByTagName("source")[0]
 
-    savePlaybackPosition(PlayerSource.getAttribute("src"), Player.currentTime)
+    // TODO: just save every 10 sec.
+
+    if (parseInt(Player.currentTime) % 10 == 0)
+    {
+        savePlaybackPosition(PlayerSource.getAttribute("src"), Player.currentTime)
+    }
 
     // console.log(Player.currentTime);
     // console.log(Player.duration);
