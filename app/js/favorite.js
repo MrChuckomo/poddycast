8c8dc8fd8211f5190b01470f9d2c2478924d8680
Alexander Kettler <alexander.kettler91@gmail.com>
2020-05-24 21:38:06 +0200

    Update favorite.js
    # linting adjustments

--- app/js/favorite.js
+++ app/js/favorite.js
@@ -1,38 +1,31 @@
+'use strict'
 
-function setFavorite(_Self, _ArtistName, _CollectioName, _Artwork30, _Artwork60, _Artwork100, _FeedUrl)
-{
-    var Feed =
-    {
-        "artistName": _ArtistName,
-        "collectionName": _CollectioName,
-        "artworkUrl30": _Artwork30,
-        "artworkUrl60": _Artwork60,
-        "artworkUrl100": _Artwork100,
-        "feedUrl": _FeedUrl
+function setFavorite(_Self, _ArtistName, _CollectioName, _Artwork30, _Artwork60, _Artwork100, _FeedUrl) {
+    let Feed = {
+        'artistName': _ArtistName,
+        'artworkUrl100': _Artwork100,
+        'artworkUrl30': _Artwork30,
+        'artworkUrl60': _Artwork60,
+        'collectionName': _CollectioName,
+        'feedUrl': _FeedUrl
     }
 
     _Self.innerHTML = s_HeartFilled
-    _Self.classList.add("set-favorite")
+    _Self.classList.add('set-favorite')
 
-    var JsonContent = []
+    let JsonContent = []
 
-    if (fs.existsSync(getSaveFilePath()) && fs.readFileSync(getSaveFilePath(), "utf-8") != "")
-    {
-        JsonContent = JSON.parse(fs.readFileSync(getSaveFilePath(), "utf-8"))
-    }
-    else
-    {
+    if (fs.existsSync(getSaveFilePath()) && fs.readFileSync(getSaveFilePath(), 'utf-8') !== '') {
+        JsonContent = JSON.parse(fs.readFileSync(getSaveFilePath(), 'utf-8'))
+    } else {
         fs.writeFileSync(getSaveFilePath(), JSON.stringify(JsonContent))
     }
 
-    if (!isAlreadySaved(_FeedUrl))
-    {
+    if (!isAlreadySaved(_FeedUrl)) {
         JsonContent.push(Feed)
     }
 
     fs.writeFileSync(getSaveFilePath(), JSON.stringify(JsonContent))
-
     addToSettings(_CollectioName, _FeedUrl)
-
     setItemCounts()
 }
