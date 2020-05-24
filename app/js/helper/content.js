d8d9788feb762f200d2ed827857aad8d668881b9
Alexander Kettler <alexander.kettler91@gmail.com>
2020-05-24 21:43:25 +0200

    Update content.js
    # linting adjustments

--- app/js/helper/content.js
+++ app/js/helper/content.js
@@ -1,31 +1,27 @@
-function CContentHelper ()
-{
+'use strict'
+
+function CContentHelper () {
     // ---------------------------------------------------------------------------------------------------------------------
     // RIGHT COLUMN
     // ---------------------------------------------------------------------------------------------------------------------
 
-    this.clearContent = function () 
-    {
-        document.getElementById("list").innerHTML = ""
+    this.clearContent = function () {
+        document.getElementById('list').innerHTML = ''
     }
 
-    this.clearListSelection = function ()
-    {
-        var ListItems = document.getElementById("list").getElementsByTagName("li")
+    this.clearListSelection = function () {
+        let ListItems = document.getElementById('list').getElementsByTagName('li')
 
-        for (var i = 0; i < ListItems.length; i++)
-        {
-            ListItems[i].classList.remove("select-episode")
+        for (let i = 0; i < ListItems.length; i++) {
+            ListItems[i].classList.remove('select-episode')
         }
     }
 
-    this.setHeader = function (_Title) 
-    {
-        var Header = document.getElementById("content-right").getElementsByTagName("h1")[0]
+    this.setHeader = function (_Title) {
+        let Header = document.getElementById('content-right').getElementsByTagName('h1')[0]
 
         Header.innerHTML = _Title
     }
 }
-
 
 module.exports = CContentHelper
