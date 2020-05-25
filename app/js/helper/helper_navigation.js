7e081258fbd2461f232305be5d6efd153f8f1da0
Alexander Kettler <alexander.kettler91@gmail.com>
2020-05-25 17:00:02 +0200

    Update helper_navigation.js
    # linting adjustments

--- app/js/helper/helper_navigation.js
+++ app/js/helper/helper_navigation.js
@@ -1,51 +1,40 @@
+'use strict'
 
 // ---------------------------------------------------------------------------------------------------------------------
 // LEFT COLUMN
 // ---------------------------------------------------------------------------------------------------------------------
 
-function setItemCounts()
-{
-    var NewEpisodesCount       = document.getElementById("menu-episodes").getElementsByClassName("menu-count")[0]
+function setItemCounts() {
+    let NewEpisodesCount = document.getElementById('menu-episodes').getElementsByClassName('menu-count')[0]
 
-    if (fs.existsSync(getPlaylistFilePath()) && fs.readFileSync(getNewEpisodesSaveFilePath(), "utf-8") != "")
-    {
-        NewEpisodesCount.innerHTML = JSON.parse(fs.readFileSync(getNewEpisodesSaveFilePath(), "utf-8")).length
-    }
-    else
-    {
+    if (fs.existsSync(getPlaylistFilePath()) && fs.readFileSync(getNewEpisodesSaveFilePath(), 'utf-8') !== '') {
+        NewEpisodesCount.innerHTML = JSON.parse(fs.readFileSync(getNewEpisodesSaveFilePath(), 'utf-8')).length
+    } else {
         NewEpisodesCount.innerHTML = 0
     }
 
-    var FavoritesCount       = document.getElementById("menu-favorites").getElementsByClassName("menu-count")[0]
+    let FavoritesCount = document.getElementById('menu-favorites').getElementsByClassName('menu-count')[0]
 
-    if (fs.existsSync(getPlaylistFilePath()) && fs.readFileSync(getSaveFilePath(), "utf-8") != "")
-    {
-        FavoritesCount.innerHTML = JSON.parse(fs.readFileSync(getSaveFilePath(), "utf-8")).length
-    }
-    else
-    {
+    if (fs.existsSync(getPlaylistFilePath()) && fs.readFileSync(getSaveFilePath(), 'utf-8') !== '') {
+        FavoritesCount.innerHTML = JSON.parse(fs.readFileSync(getSaveFilePath(), 'utf-8')).length
+    } else {
         FavoritesCount.innerHTML = 0
     }
 }
 
-function clearPlaylists()
-{
-    var AllInputFields = document.getElementById("playlists").getElementsByTagName("input")
+function clearPlaylists() {
+    let AllInputFields = document.getElementById('playlists').getElementsByTagName('input')
 
-    for (var i = 0; i < AllInputFields.length; i++)
-    {
-        if (!AllInputFields[i].disabled)
-        {
+    for (let i = 0; i < AllInputFields.length; i++) {
+        if (!AllInputFields[i].disabled) {
             clearRenameFocus(AllInputFields[i])
         }
     }
 }
 
-function clearRenameFocus(_Self)
-{
-    if (_Self.value == "")
-    {
-        var HeaderName = document.getElementById("content-right-header").getElementsByTagName("h1")[0].innerHTML
+function clearRenameFocus(_Self) {
+    if (_Self.value === '') {
+        let HeaderName = document.getElementById('content-right-header').getElementsByTagName('h1')[0].innerHTML
 
         _Self.value = HeaderName
     }
@@ -53,12 +42,10 @@ function clearRenameFocus(_Self)
     renamePlaylistInline(_Self)
 }
 
-function renamePlaylistInline(_Self)
-{
-    if (_Self.value != null && _Self.value != "")
-    {
-        var HeaderName = document.getElementById("content-right-header").getElementsByTagName("h1")[0].innerHTML
-        var NewName = _Self.value
+function renamePlaylistInline(_Self) {
+    if (_Self.value !== null && _Self.value !== '') {
+        let HeaderName = document.getElementById('content-right-header').getElementsByTagName('h1')[0].innerHTML
+        let NewName = _Self.value
 
         setPlaylistName(HeaderName, NewName)
         showPlaylistContent(_Self.parentElement)
@@ -67,36 +54,24 @@ function renamePlaylistInline(_Self)
     }
 }
 
-function renamePlaylistInEdit(_Self)
-{
-    if (_Self.value != null && _Self.value != "")
-    {
-        var SelectionName = document.getElementById("playlists").getElementsByClassName("selected")[0].getElementsByTagName("input")[0].value
-        var NewName = _Self.value
+function renamePlaylistInEdit(_Self) {
+    if (_Self.value !== null && _Self.value !== '') {
+        let SelectionName = document.getElementById('playlists').getElementsByClassName('selected')[0].getElementsByTagName('input')[0].value
+        let NewName = _Self.value
 
         setPlaylistName(SelectionName, NewName)
-        document.getElementById("playlists").getElementsByClassName("selected")[0].getElementsByTagName("input")[0].value = NewName
-
-        console.log(_Self.parentElement.getElementsByTagName("button")[0].getAttribute("onclick"));
-
-        _Self.parentElement.getElementsByTagName("button")[0].setAttribute("onclick", "deletePlaylist('" + NewName + "')")
-
-        console.log(_Self.parentElement.getElementsByTagName("button")[0].getAttribute("onclick"));
+        document.getElementById('playlists').getElementsByClassName('selected')[0].getElementsByTagName('input')[0].value = NewName
+        _Self.parentElement.getElementsByTagName('button')[0].setAttribute('onclick', 'deletePlaylist("' + NewName + '")')
     }
 }
 
-function setPlaylistName(_OldName, _NewName)
-{
-    if (fs.existsSync(getPlaylistFilePath()) && fs.readFileSync(getPlaylistFilePath(), "utf-8") != "")
-    {
-        JsonContent = JSON.parse(fs.readFileSync(getPlaylistFilePath(), "utf-8"))
+function setPlaylistName(_OldName, _NewName) {
+    if (fs.existsSync(getPlaylistFilePath()) && fs.readFileSync(getPlaylistFilePath(), 'utf-8') !== '') {
+        JsonContent = JSON.parse(fs.readFileSync(getPlaylistFilePath(), 'utf-8'))
 
-        for (var i = 0; i < JsonContent.length; i++)
-        {
-            if (JsonContent[i].playlistName == _OldName)
-            {
+        for (let i = 0; i < JsonContent.length; i++) {
+            if (JsonContent[i].playlistName === _OldName) {
                 JsonContent[i].playlistName = _NewName
-
                 break
             }
         }
@@ -105,19 +80,11 @@ function setPlaylistName(_OldName, _NewName)
     }
 }
 
-function setGridLayout(_List, _Enable)
-{
-    if (_Enable)
-    {
-        _List.classList.add("grid-layout")
-        // _List.style.display = "grid"
-        // _List.style.gridTemplateColumns = "1fr 1fr 1fr"
-    }
-    else
-    {
-        _List.classList.remove("grid-layout")
-        // _List.style.display = null
-        // _List.style.gridTemplateColumns = null
+function setGridLayout(_List, _Enable) {
+    if (_Enable) {
+        _List.classList.add('grid-layout')
+    } else {
+        _List.classList.remove('grid-layout')
     }
 }
 
@@ -125,41 +92,36 @@ function setGridLayout(_List, _Enable)
 // RIGHT COLUMN
 // ---------------------------------------------------------------------------------------------------------------------
 
-function setHeaderViewAction(_Mode)
-{
-    switch (_Mode)
-    {
-        case "list":
-            document.getElementById("content-right-header-actions").innerHTML = s_ListView
-            document.getElementById("content-right-header-actions").getElementsByTagName("svg")[0].setAttribute("onclick", "toggleList('list')")
-            break;
+function setHeaderViewAction(_Mode) {
+    switch (_Mode) {
+    case 'list':
+        document.getElementById('content-right-header-actions').innerHTML = s_ListView
+        document.getElementById('content-right-header-actions').getElementsByTagName('svg')[0].setAttribute('onclick', 'toggleList("list")')
+        break;
 
-        case "grid":
-            document.getElementById("content-right-header-actions").innerHTML = s_GridView
-            document.getElementById("content-right-header-actions").getElementsByTagName("svg")[0].setAttribute("onclick", "toggleList('grid')")
-            break;
+    case 'grid':
+        document.getElementById('content-right-header-actions').innerHTML = s_GridView
+        document.getElementById('content-right-header-actions').getElementsByTagName('svg')[0].setAttribute('onclick', 'toggleList("grid")')
+        break;
 
-        default: document.getElementById("content-right-header-actions").innerHTML = ""; break;
+    default: document.getElementById('content-right-header-actions').innerHTML = ''; break;
     }
 }
 
-function toggleList(_View)
-{
-    switch (_View)
-    {
-        case "list":
-            var List = document.getElementById("list")
-            setGridLayout(List, false)
-            setHeaderViewAction("grid")
-            break;
+function toggleList(_View) {
+    let List = document.getElementById('list')
+    switch (_View) {
+    case 'list':
+        setGridLayout(List, false)
+        setHeaderViewAction('grid')
+        break;
 
-        case "grid":
-            var List = document.getElementById("list")
-            setGridLayout(List, true)
-            setHeaderViewAction("list")
-            break;
+    case 'grid':
+        setGridLayout(List, true)
+        setHeaderViewAction('list')
+        break;
 
-        default: break;
+    default: break;
     }
 }
 
@@ -167,44 +129,35 @@ function toggleList(_View)
 // MENU & PLAYLISTS
 // ---------------------------------------------------------------------------------------------------------------------
 
-function clearMenuSelection()
-{
-    var Menu      = document.getElementById("menu")
-    var ListItems = Menu.getElementsByTagName("li")
-    var Playlists = document.getElementById("playlists").getElementsByTagName("li")
+function clearMenuSelection() {
+    let Menu = document.getElementById('menu')
+    let ListItems = Menu.getElementsByTagName('li')
+    let Playlists = document.getElementById('playlists').getElementsByTagName('li')
 
-    for (var i = 0; i < ListItems.length; i++)
-    {
-        ListItems[i].classList.remove("selected")
+    for (let i = 0; i < ListItems.length; i++) {
+        ListItems[i].classList.remove('selected')
     }
 
-    for (var i = 0; i < Playlists.length; i++)
-    {
-        Playlists[i].classList.remove("selected")
+    for (let i = 0; i < Playlists.length; i++) {
+        Playlists[i].classList.remove('selected')
     }
 }
 
 
-function dragToPlaylist(_PlaylistName, _PodcastName)
-{
+function dragToPlaylist(_PlaylistName, _PodcastName) {
     addToPlaylist(_PlaylistName, _PodcastName)
 }
 
-function addToPlaylist(_PlaylistName, _PodcastName)
-{
-    var JsonContent = JSON.parse(fs.readFileSync(getPlaylistFilePath(), "utf-8"))
+function addToPlaylist(_PlaylistName, _PodcastName) {
+    let JsonContent = JSON.parse(fs.readFileSync(getPlaylistFilePath(), 'utf-8'))
 
-    for (var i = 0; i < JsonContent.length; i++)
-    {
-        if (JsonContent[i].playlistName == _PlaylistName)
-        {
-            var PodcastList = JsonContent[i].podcastList
+    for (let i = 0; i < JsonContent.length; i++) {
+        if (JsonContent[i].playlistName === _PlaylistName) {
+            let PodcastList = JsonContent[i].podcastList
 
-            if (!isAlreadyInPlaylist(_PlaylistName, _PodcastName))
-            {
+            if (!isAlreadyInPlaylist(_PlaylistName, _PodcastName)) {
                 PodcastList.push(_PodcastName)
             }
-
             break
         }
     }
@@ -212,21 +165,18 @@ function addToPlaylist(_PlaylistName, _PodcastName)
     fs.writeFileSync(getPlaylistFilePath(), JSON.stringify(JsonContent))
 }
 
-function removeFromPlaylist(_PlaylistName, _PodcastName)
-{
-    var JsonContent = JSON.parse(fs.readFileSync(getPlaylistFilePath(), "utf-8"))
+function removeFromPlaylist(_PlaylistName, _PodcastName) {
+    let JsonContent = JSON.parse(fs.readFileSync(getPlaylistFilePath(), 'utf-8'))
 
-    for (var i = 0; i < JsonContent.length; i++)
-    {
-        if (JsonContent[i].playlistName == _PlaylistName)
-        {
-            var PodcastList = JsonContent[i].podcastList
+    for (let i = 0; i < JsonContent.length; i++) {
+        if (JsonContent[i].playlistName === _PlaylistName) {
+            let PodcastList = JsonContent[i].podcastList
 
-            if (isAlreadyInPlaylist(_PlaylistName, _PodcastName))
-            {
-                for (var j = PodcastList.length - 1; j >= 0 ; j--)
-                {
-                    if(PodcastList[j] == _PodcastName) { PodcastList.splice(j, 1) }
+            if (isAlreadyInPlaylist(_PlaylistName, _PodcastName)) {
+                for (let j = PodcastList.length - 1; j >= 0 ; j--) {
+                    if(PodcastList[j] === _PodcastName) {
+                        PodcastList.splice(j, 1)
+                    }
                 }
             }
 
@@ -237,14 +187,11 @@ function removeFromPlaylist(_PlaylistName, _PodcastName)
     fs.writeFileSync(getPlaylistFilePath(), JSON.stringify(JsonContent))
 }
 
-function deletePlaylist(_PlaylistName)
-{
-    var JsonContent  = JSON.parse(fs.readFileSync(getPlaylistFilePath(), "utf-8"))
+function deletePlaylist(_PlaylistName) {
+    let JsonContent = JSON.parse(fs.readFileSync(getPlaylistFilePath(), 'utf-8'))
 
-    for (var i = 0; i < JsonContent.length; i++)
-    {
-        if (_PlaylistName == JsonContent[i].playlistName)
-        {
+    for (let i = 0; i < JsonContent.length; i++) {
+        if (_PlaylistName === JsonContent[i].playlistName) {
             JsonContent.splice(i, 1)
             break
         }
