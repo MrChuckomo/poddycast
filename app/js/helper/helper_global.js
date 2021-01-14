3375be963c337acc668ba5d064cfc834951a2e55
paologiua <46423764+paologiua@users.noreply.github.com>
2021-01-14 21:18:55 +0100

    Update helper_global.js

--- app/js/helper/helper_global.js
+++ app/js/helper/helper_global.js
@@ -162,7 +162,60 @@ function setTitlebarOnWin() {
                 menuBarVisibility = !menuBarVisibility
                 setMenuBarVisibility(menuBarVisibility);
             }
-        })
+        });
+        
+        titlebar.updateStyles = function () {
+            const color_1 = require("./node_modules/custom-electron-titlebar/lib/common/color");
+            const dom_1 = require("./node_modules/custom-electron-titlebar/lib/common/dom");
+            const INACTIVE_FOREGROUND_DARK = color_1.Color.fromHex('#222222');
+            const ACTIVE_FOREGROUND_DARK = color_1.Color.fromHex('#333333');
+            const INACTIVE_FOREGROUND = color_1.Color.fromHex('#EEEEEE');
+            const ACTIVE_FOREGROUND = color_1.Color.fromHex('#FFFFFF');
+
+            if (this.titlebar) {
+                if (this.isInactive) {
+                    dom_1.addClass(this.titlebar, 'inactive');
+                }
+                else {
+                    dom_1.removeClass(this.titlebar, 'inactive');
+                }
+                const titleBackground = this.isInactive && this._options.unfocusEffect
+                    ? this._options.backgroundColor.lighten(.16)
+                    : this._options.backgroundColor;
+                this.titlebar.style.backgroundColor = titleBackground.toString();
+                let titleForeground;
+                if (titleBackground.isLighter()) {
+                    dom_1.addClass(this.titlebar, 'light');
+                    titleForeground = this.isInactive && this._options.unfocusEffect
+                        ? INACTIVE_FOREGROUND_DARK
+                        : ACTIVE_FOREGROUND_DARK;
+                }
+                else {
+                    dom_1.removeClass(this.titlebar, 'light');
+                    titleForeground = this.isInactive && this._options.unfocusEffect
+                        ? INACTIVE_FOREGROUND
+                        : ACTIVE_FOREGROUND;
+                }
+                this.titlebar.style.color = titleForeground.toString();
+                const backgroundColor = this._options.backgroundColor.darken(.16);
+                const foregroundColor = backgroundColor.isLighter()
+                    ? INACTIVE_FOREGROUND_DARK
+                    : INACTIVE_FOREGROUND;
+                const bgColor = !this._options.itemBackgroundColor || this._options.itemBackgroundColor.equals(backgroundColor)
+                    ? new color_1.Color(new color_1.RGBA(0, 0, 0, .14))
+                    : this._options.itemBackgroundColor;
+                const fgColor = bgColor.isLighter() ? ACTIVE_FOREGROUND_DARK : ACTIVE_FOREGROUND;
+                if (this.menubar) {
+                    this.menubar.setStyles({
+                        backgroundColor: backgroundColor,
+                        foregroundColor: foregroundColor,
+                        selectionBackgroundColor: bgColor,
+                        selectionForegroundColor: fgColor,
+                        separatorColor: foregroundColor
+                    });
+                }
+            }
+        }
     }
 }
 
